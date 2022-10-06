943a7dff2301e9e9f808766c03381e7df179360e
Micheal A <aad3rinto@gmail.com>
2022-10-06 11:37:09 +0000

    break out components

---
+++ build/precache-manifest.0edfccbfce99f37b57c566df299d5dc6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dd9a83f5d789270f9448bd9f3f54974",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c45f18b7b07026c93b12",
+    "url": "/static/js/2.f2d3141e.chunk.js"
+  },
+  {
+    "revision": "ad0b83fbdf791dcdf130",
+    "url": "/static/js/main.db67424d.chunk.js"
+  },
+  {
+    "revision": "8c6b7e36852fa9d9d9e9",
+    "url": "/static/js/runtime-main.a77248bd.js"
+  }
+]);
